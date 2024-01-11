751ec482494077ca655c8e1c7228588052585e45
Michael Howard <silverdragon9.8.13@gmail.com>
2024-01-10 23:08:10 -0600

    I created the basic HomePage (index.html)

--- assignments.php
+++ assignments.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignments</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
     <header><p> Assignemts </p></header>
--- index.php
+++ index.php
@@ -7,45 +7,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello!</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
+<div id="wrapper">
 <!--  Michael Howard && T worked on this assignment  -->
 <!-- <h1>Hello <?php echo $name ?></h1>              -->
 
     <header>
         <h1>&nbsp;&nbsp;&nbsp;&nbsp; <!-- adding space -->
-            Home
+            Home Page
         </h1>
-    </header><br><br><br><br><hr>
+    </header><br><br><br><br><hr width="110%" align="left">
 
 <!---Navigation Bar------------------------------------------------->
     <nav>
         <ul>
-            <li> <a href="index.php">Home</a> </li>
+            <li> <a href="index.php">Home</a></li>
             <li> <a href="assignments.php">Assignments</a></li>
+            <li> <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Click for Rick Roll!</a></li> 
         </ul>
     </nav><hr><br><br>
 <!------------------------------------------------------------------>
     <main>
-        <center>
-        <div>
-            <h4> Question 1: </h4>
-            <p></p>
+        <div align="left">
+            <h3> Question 1: Why are you taking this class? </h3>
+            <pre><p>
+                    I am taking this class because I am very interested in learning
+                back-end web development Learning databases and API's sounds very
+                useful and a great thing to learn!
+            </p></pre>
         </div>
         <br>
-        <div>
-            <h4> Question 2: </h4>
-            <p></p>
+        <div align="left">
+            <h3> Question 2: What do you want to take away from this class?</h3>
+            <p>
+                    I would like to have a solid understanding of databases, github,
+                and php. That way I can make websites that are more useful!
+            </p>
         </div>
-
-        </center>
     </main>
     <br><br><br><br><br><br><br><br>
     <footer>
         <center>
-            <h6><a href="https://github.com/Silverdragon977/hello-world.git"> GitHub Repository Code </a> <h6>
+            <h6><a href="https://github.com/Silverdragon977/hello-world.git"><i> GitHub Repository Code <i></a> <h6>
         </center>
     </footer>
+</div>
 </body>
 </html>
 
