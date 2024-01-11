adcd1f6004b7c9c106e8fad026433cd254a665e1
Michael Howard <silverdragon9.8.13@gmail.com>
2024-01-10 20:53:39 -0600

    update

--- index.php
+++ index.php
@@ -6,10 +6,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello Michael</title>
+    <title>Hello!</title>
 </head>
 <body>
-    <h1>Hello <?php echo $name ?></h1>
+<!--  Michael Howard && T worked on this assignment  -->
+<!-- <h1>Hello <?php echo $name ?></h1>              -->
+
+    <header>
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp; <!-- adding space -->
+            Home
+        </h1>
+    </header><br><br><br><br><hr>
+
+<!---Navigation Bar------------------------------------------------->
+    <nav>
+        <ul>
+            <li> <a href="index.php">Home</a> </li>
+            <li> <a href="assignments.php">Assignments</a></li>
+        </ul>
+    </nav><hr><br><br>
+<!------------------------------------------------------------------>
+    <main>
+        <center>
+        <div>
+            <h4> Question 1: </h4>
+            <p></p>
+        </div>
+        <br>
+        <div>
+            <h4> Question 2: </h4>
+            <p></p>
+        </div>
+
+        </center>
+    </main>
+    <br><br><br><br><br><br><br><br>
+    <footer>
+        <center>
+            <h6><a href="https://github.com/Silverdragon977/hello-world.git"> GitHub Repository Code </a> <h6>
+        </center>
+    </footer>
 </body>
 </html>
 
